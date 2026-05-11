e8e50e2c3633a51dd217eb167130b5e2c6a976aa
jode <warzoneuser3@gmail.com>
2026-05-11 14:12:56 +0800

    just an update

--- obj/Debug/net10.0/StallFruitsManagement.AssemblyInfo.cs
+++ obj/Debug/net10.0/StallFruitsManagement.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("StallFruitsManagement")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b17cbbd91357b08d45282ad75c33014c744bbfc1")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f450a6368a10e5f6418e1eb7086dcf990079d19a")]
 [assembly: System.Reflection.AssemblyProductAttribute("StallFruitsManagement")]
 [assembly: System.Reflection.AssemblyTitleAttribute("StallFruitsManagement")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- wwwroot/js/site.js
+++ wwwroot/js/site.js
@@ -1,4 +1,41 @@
 ﻿// Please see documentation at https://learn.microsoft.com/aspnet/core/client-side/bundling-and-minification
 // for details on configuring this project to bundle and minify static web assets.
 
-// Write your JavaScript code.
+// Sidebar toggle for small screens
+document.addEventListener("DOMContentLoaded", function () {
+  var toggle = document.querySelector(".sidebar-toggle");
+  var overlay = document.getElementById("sidebarOverlay");
+
+  function openSidebar() {
+    document.body.classList.add("sidebar-open");
+    if (overlay) overlay.classList.add("show");
+  }
+
+  function closeSidebar() {
+    document.body.classList.remove("sidebar-open");
+    if (overlay) overlay.classList.remove("show");
+  }
+
+  if (toggle) {
+    toggle.addEventListener("click", function (e) {
+      e.preventDefault();
+      if (document.body.classList.contains("sidebar-open")) closeSidebar();
+      else openSidebar();
+    });
+  }
+
+  if (overlay) {
+    overlay.addEventListener("click", function () {
+      closeSidebar();
+    });
+  }
+
+  // Close sidebar when a nav link is clicked (mobile)
+  document.querySelectorAll(".sidebar-nav .nav-link").forEach(function (el) {
+    el.addEventListener("click", function () {
+      closeSidebar();
+    });
+  });
+});
+
+// Additional site JS can be added below
